875b565295b126d370de6d5f0189fd676d4dfbb2
mesurajyadav <mesurajyadav@outlook.com>
2023-08-07 21:33:14 +0530

    Assessment1

---
+++ 7. assesment1.sql
@@ -0,0 +1,10 @@
+
+--data type, create table and view, constraint, identity
+
+--1. create emp table (id, name) with department (id, name) with PK and FK
+--2. add a column called age and gender in employee table and rename name to EmployeeName
+--3. create 2 views, for female employees and for employees having name starting with A
+--4. put a check constraint on the age column age should be between 0 to 150.
+--5. change employee id into identity column keeping all old data intact.
+--6. update the view and add Ms. infront of all names of female and Mr. infront of all names of male employees
+--7. add date of birth and date of joning with zone information for all employees
